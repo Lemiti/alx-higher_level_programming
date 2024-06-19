8459f9f33b84bae06383222259deaa0e9e22c13a
Lemi Negeso <leminegeso@gmail.com>
2024-06-18 22:32:06 -0700

    edit answer

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,6 @@
 #!/bin/bash
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+const first ='C is fun';
+const second = 'Python is cool';
+const third = 'JavaScript is amazing';
+
+console.log(first + '\n' + second + '\n' + third);
