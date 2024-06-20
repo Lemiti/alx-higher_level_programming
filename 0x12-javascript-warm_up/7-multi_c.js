59e455783b09289c23efd057d935d52cc3b107ba
Lemi Negeso <leminegeso@gmail.com>
2024-06-20 14:44:45 -0700

    dongjjlk

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-a = process.argv[2];
-b = parseInt(a, 10);
-if (isNaN
+const a = process.argv[2];
+const b = parseInt(a, 10);
+let x;
+if (isNaN(b)) {
+  console.log('Missing number of occurrences');
+} else if (b > 0) {
+  for (x = 0; x < b; x++) {
+    console.log('C is fun');
+  }
+}
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
 
+const a = process.argv[2];
+const b = parseInt(a, 10);
+let x;
+let y;
+if (isNaN(b)) {
+  console.log('Missing size');
+} else if (b > 0) {
+  for (x = 0; x < b; x++) {
+    for (y = 0; y < b; y++) {
+      process.stdout.write('X');
+    }
+    console.log('');
+  }
+}
