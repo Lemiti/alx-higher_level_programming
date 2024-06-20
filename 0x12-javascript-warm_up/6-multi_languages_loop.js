dda590aab582a55a9b9b3cfcf25f0c017050fa79
Lemi Negeso <leminegeso@gmail.com>
2024-06-20 13:56:34 -0700

    edit

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 
 const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let x;
-for (x === 0; x <== 2; x++) {
+for (x = 0; x <= 2; x++) {
   console.log(myArr[x]);
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,2 +1,4 @@
 #!/usr/bin/node
-
+a = process.argv[2];
+b = parseInt(a, 10);
+if (isNaN
