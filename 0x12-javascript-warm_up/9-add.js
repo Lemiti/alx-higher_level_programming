f35785fbc12b6d10ecb4d6167aa5977d8b7b091e
Lemi Negeso <leminegeso@gmail.com>
2024-06-21 09:04:23 -0700

    9-add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
 
+const a = process.argv[2];
+const a1 = process.argv[3];
+const b = parseInt(a, 10);
+const b1 = parseInt(a1, 10);
+
+function add (b1, b) {
+  if (isNaN(b) || isNaN(b1)) {
+    console.log('NaN');
+  } else {
+    console.log(b1 + b);
+  }
+}
+add(b1, b);
