5f02144b2ad074075e0203dd2614d0e1270f0055
Lemi Negeso <leminegeso@gmail.com>
2024-06-29 00:48:50 -0700

    do 10 and 11

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const a = process.argv[2];
 const b = parseInt(a, 10);
-let c = 1;
 if (isNaN(b)) {
   console.log(1);
 } else {
-  function factorial(b) {
+  function factorial (b) {
     if (b <= 1) {
       return 1;
     } else {
-      return b*factorial(b-1);
+      return b * factorial(b - 1);
     }
   }
   console.log(factorial(b));
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,29 @@
 #!/usr/bin/node
 
 const arr = [];
-
+let a;
+let c = 0;
+const le = process.argv.length;
+for (a = 2; a < le; a++) {
+  const d = process.argv[a];
+  const num = parseInt(d, 10);
+  arr[c] = num;
+  c += 1;
+}
+let k;
+let temp;
+let j;
+for (k = c - 2; k >= 0; k--) {
+  temp = arr[k];
+  j = k + 1;
+  while (temp <= arr[j]) {
+    arr[j - 1] = arr[j];
+    j += 1;
+  }
+  arr[j - 1] = temp;
+}
+if (arr[1] == undefined || arr.length == 1) {
+  console.log(0);
+} else {
+  console.log(arr[1]);
+}
